aedd67d572d15a0c1889e08e7376e97cc625d1b0
Tonyson8245 <dongo8245@gmail.com>
2026-01-31 21:05:40 +0900

    test : 테스트 케이스 검증

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 
@@ -16,9 +15,5 @@ async function enableMocking() {
 }
 
 enableMocking().then(() => {
-  createRoot(document.getElementById("root")!).render(
-    <StrictMode>
-      <App />
-    </StrictMode>,
-  );
+  createRoot(document.getElementById("root")!).render(<App />);
 });
